481467d7ec474a47b5f02361dd839443a6f6ccb2
Mohsen Yazdandust <m.yazdandust1403@gmail.com>
2024-08-22 00:01:27 +0330

    finish up

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from .models import Student
+from .models import Student, Session, Exercise, Submitted
 
+class ChannelAdmin(admin.StackedInline):
+    model = Submitted
+    fields = ['exercise', 'answer', 'feedback']
+    readonly_fields = ['exercise', 'answer']
+    extra = 0
 
-admin.site.register(Student)
+class RuleAdmin(admin.ModelAdmin):
+    model = Student
+    fields = ['first_name', 'last_name']
+    readonly_fields = ['first_name', 'last_name']
+    inlines = [ChannelAdmin]
+    
+admin.site.register(Student, RuleAdmin)
+admin.site.register(Session)
+admin.site.register(Exercise)
+admin.site.register(Submitted)
--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+import datetime
 
 class Student(models.Model):
     username = models.CharField(max_length=255)
@@ -9,4 +10,30 @@ class Student(models.Model):
     is_accepted = models.BooleanField(default=False)
     
     def __str__(self):
-        return self.username
+        return self.first_name + " " + self.last_name
+    
+
+class Exercise(models.Model):
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
+
+
+class Session(models.Model):
+    date = models.DateField(default=datetime.date.today)
+    exercises = models.ManyToManyField(Exercise, blank=True)
+    presents = models.ManyToManyField(Student, blank=True)
+    
+    def __str__(self):
+        return str(self.date)
+    
+
+class Submitted(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    answer = models.TextField()
+    feedback = models.TextField()
+    
+    def __str__(self):
+        return str(self.exercise)
